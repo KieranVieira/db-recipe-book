121178caeb0f6476f392a934794043e0412ac394
KieranVieira <kieranvieira@live.com>
2019-02-21 17:23:28 -0500

    Added getDish helper

--- database/helpers/recipeHelper.js
+++ database/helpers/recipeHelper.js
@@ -9,7 +9,16 @@ const addDish = dish => {
         .insert(dish)
 }
 
+const getDish = id => {
+    return db
+        .from('dishes')
+        .select('dishes.name as Dish', 'dishes.id as Dish_Id', 'recipes.name as RecipeName')
+        .join('recipes', {'recipes.dish_id': 'dishes.id'})        
+        .where({ 'dishes.id': id })
+}
+
 module.exports = {
     getDishes,
-    addDish
+    addDish,
+    getDish
 }
--- server.js
+++ server.js
@@ -7,6 +7,4 @@ const server = express()
 server.use(express.json());
 server.use(helmet())
 
-db.addDish({name: 'Cake'}).then(dishId => console.log(dishId)).catch(error => console.log(error))
-
 module.exports = server;
